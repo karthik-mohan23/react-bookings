71f67b5a474528d744b2a3a3d362e5583d634d20
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-06-04 14:44:05 +0530

    sign up logic

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4fa_jzIw0uYwlpS_As0Jh4Xn4IKIOSks",
+  authDomain: "test1-27382.firebaseapp.com",
+  projectId: "test1-27382",
+  storageBucket: "test1-27382.appspot.com",
+  messagingSenderId: "523681207541",
+  appId: "1:523681207541:web:bff9d345dce19e615461bc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const auth = getAuth(app);
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,83 @@
+import { Input } from "@nextui-org/input";
+import { Button, Divider } from "@nextui-org/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
+
 function SignUp() {
-  return <div>SignUp</div>;
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    // if (!fullname || !email || !password) return;
+    console.log({ fullname, email, password });
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+  return (
+    <section className="pt-9">
+      <h2 className="text-center text-2xl font-bold pb-9">
+        Log in to your account.
+      </h2>
+      <form
+        onSubmit={handleLogin}
+        className="flex flex-col max-w-sm mx-auto  gap-4">
+        <Input
+          type="text"
+          variant="bordered"
+          label="Full Name"
+          placeholder="Enter your full name"
+          value={fullname}
+          onChange={(e) => setFullname(e.target.value)}
+        />
+        <Input
+          type="email"
+          variant="bordered"
+          label="Email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          variant="bordered"
+          label="Password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="px-2 flex justify-between">
+          <p>
+            <span>Already a member? </span>
+            <Link
+              to="/sign-in"
+              className="underline text-blue-500 hover:text-blue-600 duration-250">
+              login
+            </Link>
+          </p>
+          <span className="  hover:text-blue-600 duration-250 cursor-pointer">
+            <Link to="/forgot-password"> Forgot Password?</Link>
+          </span>
+        </div>
+        <Button type="submit" color="primary">
+          SIGN UP
+        </Button>
+        <Divider className="my-4" />
+        <Button color="danger">CONTINUE WITH GOOGLE</Button>
+      </form>
+    </section>
+  );
 }
 export default SignUp;
